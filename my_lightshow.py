78b6bae882791d85b84ced610bb91782cf59da1f
Zed <113933520+Zedbits@users.noreply.github.com>
2022-09-21 13:35:12 -0400

    Update my_lightshow.py

--- my_lightshow.py
+++ my_lightshow.py
@@ -14,9 +14,9 @@ Return: none
 """
 def sparkle(color1, color2, loop = 10, delay=0.1):
   for outer in range(loop):
-    np.fill(background)
+    np.fill(color1)
     for i in range(np.n / 4):
-      np[random.randint(0, np.n-1)] = foreground
+      np[random.randint(0, np.n-1)] = color2
     np.show()
     time.sleep(delay)
 '''
