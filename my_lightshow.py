852ed1d9bb86526c9a842f337c884d000951bcf9
Zed <113933520+Zedbits@users.noreply.github.com>
2022-09-27 13:55:37 -0400

    Update my_lightshow.py

--- my_lightshow.py
+++ my_lightshow.py
@@ -2,13 +2,13 @@ import board
 import neopixel
 import time
 import random
-BRIGHTNESS = .5
+BRIGHTNESS = 1.0
 num_pixels = 10
 np = neopixel.NeoPixel(board.NEOPIXEL, num_pixels, brightness=BRIGHTNESS, auto_write=False)
 """
 Function: sparkle
 Description: a chosen foreground color flashes randomly on a chosen background color.
-Parameters: color1&2(tuples) - color1 is background, color2 is foreground. delay(floating
+Parameters: color1(tuple) - color1 is background, color2(tuple)is foreground. delay(floating
 point) - time between flashes. loop (int) is the time the light flashes
 Return: none
 """
@@ -24,24 +24,22 @@ Function: chase
 Description: The foreground color chases other instances of the foreground color on a
 backdrop of
 the background color
-Parameters: color1&2(tuple) - color1 is background, color2 is foreground. loop(int) is how much
-the
-color moves around. count(int) is the space between chasing pixels. delay(floating
+Parameters: color1(tuple) is background, color2(tuple) is foreground. loop(int) is how much
+the color moves around. count(int) is the space between chasing pixels. delay(floating
 point)
 is the time it takes to move color
 Return value: none
 '''
-def chase(color1, color2, loop=20, count=3, delay=0.1):
+def chase(color1, color2, loop=10, delay=0.1):
   result = 0
-  for outer in range(count*loop):
+  for outer in range(loop):
     np.fill(color1)
-    for i in range(num_pixels):
-      if i % count == result:
+    for i in range(np.n):
+      if i % 3 == result:
         np[i] = color2
-  np.show()
-  time.sleep(delay)
-  result += 1
-  result %= count
+    np.show()
+    time.sleep(delay)
+    result = (result + 1) % 3
 '''
 Function: fade_in
 Description: This function fades a color in from black.
@@ -89,40 +87,62 @@ def fade_out(color, delay=0.01):
     print('r - {}, g - {}, b - {}'.format(int(r), int(g), int(b)))
     np.show()
     time.sleep(delay)
+'''
+Function: Fire
+Description: Creates a flame effect on a strip of neopixels
+Parameters: background(tuple): base color for flame, foreground(tuple): color used to flash
+return value: none
+'''
+def fire(background, foreground):
+    why = random.random()/5
+    for j in range(20):
+        intensity = random.random() * 0.7 + 0.3
+        i_background = [int(i * intensity) for i in background]
+        np.fill(i_background)
+        for i in range(random.randint(2, int(num_pixels/5))):
+            intensity = random.random() * 0.7 + 0.3
+            i_foreground = [int(i * intensity) for i in foreground]
+            np[random.randint(0, num_pixels-1)] = i_foreground
+        np.show()
+        time.sleep(why)
+'''
+Function: lightning
+Description: Creates a lightning effect
+Parameters: background(tuple): base color for the lightning, foreground(tuple): color used
+to flash
+return value: none
+'''
+def light(background, foreground):
+    why = random.random()/20
+    for j in range(20):
+        intensity = random.random() * 0.7 + 0.3
+        i_background = [int(i * intensity) for i in background]
+        np.fill(i_background)
+        for i in range(random.randint(2, int(num_pixels/5))):
+            intensity = random.random() * 0.7 + 0.3
+            i_foreground = [int(i * intensity) for i in foreground]
+            np[random.randint(0, num_pixels-1)] = i_foreground
+        np.show()
+        time.sleep(why)
 while True:
-# fades black to red
-fade_in((255, 1, 1))
-# starts sparkling with yellow color
-sparkle((255, 0, 0), (255, 211, 0))
-# fades to black
-fade_out((255, 1, 1))
-# fades in yellow
-fade_in((255, 211, 1))
-# chases with red
-chase((255, 211, 1), (255, 1, 1), loop=10, count=4, delay=.1)
-# fades to black
-fade_out((255, 211, 1))
-# fades in green
-fade_in((103, 228, 1))
-# sparkles with blue color
-sparkle((103, 228, 1), (11, 68, 172))
-# fades out
-fade_out((103, 228, 1))
-# fades in blue
-fade_in((11, 68, 172))
-# chases with green
-chase((11, 68, 172), (103, 228, 1), loop=10, count=3, delay=.1)
-# fades out
-fade_out((11, 68, 172))
-# fades in cyan
-fade_in((1, 167, 130))
-# sparkles with magenta
-sparkle((1, 167, 130), (204, 1, 119))
-# fades out
-fade_out((1, 167, 130))
-# fades in orange
-fade_in((255, 116, 1))
-# chases with red
-chase((255, 116, 1), (255, 1, 1), loop=10, count=2, delay=.1)
-# fades out
-fade_out((255, 116, 1))
+    # fades into orange
+    fade_in((179, 98, 1))
+    # lightning effect with orange background and green lightning
+    fire((179, 98, 1), (179, 98, 1))
+    light((1, 255, 1), (1, 255, 1))
+    fire((179, 98, 1), (179, 98, 1))
+    # fades out from orange
+    fade_out((179, 98, 1))
+    # fades into purple
+    fade_in((144, 46, 187))
+    # chases black color
+    chase((144, 46, 187), (1, 1, 1), 50, .07)
+    # fades out
+    fade_out((144, 46, 187))
+    # fades into red
+    fade_in((255, 1, 1))
+    # sparkles purple on a red background
+    sparkle((255, 1, 1), (98, 1, 179), 50)
+    # fades out
+    fade_out((98, 1, 179))
+    
